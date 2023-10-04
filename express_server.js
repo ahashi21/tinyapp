f7efa7523a8c1c994361c4b88f9bcbeee4a81485
ahashi21 <ahmedxusseinx@gmail.com>
2023-10-03 20:12:08 -0400

    added post and stuff

--- express_server.js
+++ express_server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser"); // Import cookie-parser middleware
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // Use cookie-parser middleware
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
-
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -28,7 +29,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies.username }; // Pass username to the template
   res.render("urls_index", templateVars);
 });
 
@@ -36,17 +37,15 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// Updated route for /urls/:id
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-// Handle short URL redirection
 app.get("/u/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
-  
+
   if (longURL) {
     res.redirect(longURL);
   } else {
@@ -54,7 +53,6 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-// Updated route for deleting URLs
 app.post("/urls/:id/delete", (req, res) => {
   const shortURL = req.params.id;
   delete urlDatabase[shortURL];
@@ -62,10 +60,17 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
+  console.log(req.body);
   const shortURL = generateRandomString(6);
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`); // Redirect to the show page for the newly created URL
+  res.redirect(`/urls/${shortURL}`);
+});
+
+// Handle the login POST request
+app.post("/login", (req, res) => {
+  const { username } = req.body; // Get the username from the request body
+  res.cookie("username", username); // Set the username as a cookie
+  res.redirect("/urls"); // Redirect back to the /urls page
 });
 
 function generateRandomString(length) {
