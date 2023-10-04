384f21af643ffb8c3ed2e8fd460f62429cd158f4
ahashi21 <ahmedxusseinx@gmail.com>
2023-10-03 21:29:26 -0400

    logout

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
@@ -29,7 +29,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// Updated route for /urls
 app.get("/urls", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
@@ -42,13 +41,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// Updated route for /urls/:id
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-// Handle short URL redirection
 app.get("/u/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
@@ -60,14 +57,6 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-// Handle user login and set the username cookie
-app.post("/login", (req, res) => {
-  const { username } = req.body;
-  res.cookie("username", username);
-  res.redirect("/urls");
-});
-
-// Updated route for deleting URLs
 app.post("/urls/:id/delete", (req, res) => {
   const shortURL = req.params.id;
   delete urlDatabase[shortURL];
@@ -78,7 +67,18 @@ app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   const shortURL = generateRandomString(6);
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`); // Redirect to the show page for the newly created URL
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 function generateRandomString(length) {
